65570bc8d2ce0803de6c7a97723e8fb081fecb42
Adrianus Kleemans <a.kleemans@gmail.com>
2018-12-30 12:32:55 +0100

    Added health bar

--- src/MainMenuScene.js
+++ src/MainMenuScene.js
@@ -6,18 +6,10 @@ var MainMenuScene = new Phaser.Class({
     },
 
   preload: function () {
-    // pre-loading assets for game
-    /*
-    this.load.spritesheet('player', 'assets/img/player.png', {frameWidth: 46, frameHeight: 46});
-    this.load.spritesheet('crystal', 'assets/img/save-crystal-animated.png', {frameWidth: 32, frameHeight: 32});
-    this.load.spritesheet('invader', 'assets/invader.png', {frameWidth: 32, frameHeight: 32});
-    this.load.spritesheet('spider', 'assets/img/spider.png', {frameWidth: 24, frameHeight: 54});
-    this.load.image('heart-full', 'assets/img/heart-full.png', {frameWidth: 6, frameHeight: 7});
-    this.load.image('heart-half', 'assets/img/heart-half.png', {frameWidth: 6, frameHeight: 7});
-    this.load.image('cog-yellow', 'assets/img/cog-yellow.png', {frameWidth: 15, frameHeight: 15});
-    */
-
     this.load.image('intro', 'assets/img/intro.png');
+    this.load.image('health', 'assets/img/health.png');
+    this.load.image('money', 'assets/img/money.png');
+    this.load.image('wave', 'assets/img/wave.png');
 
     this.load.image('level1', 'assets/img/level1.png');
     this.load.image('tower1', 'assets/img/tower1.png');
@@ -25,8 +17,11 @@ var MainMenuScene = new Phaser.Class({
     this.load.image('bullet', 'assets/img/bullet.png', {frameWidth: 7, frameHeight: 7});
 
     this.load.spritesheet('enemy1', 'assets/img/enemy1.png', {frameWidth: 32, frameHeight: 32});
+    //this.load.image('healthbar', 'assets/img/healthbar.png', {frameWidth: 16, frameHeight: 2});
+    this.load.image('healthbar-green', 'assets/img/healthbar-green.png');
+    this.load.image('healthbar-red', 'assets/img/healthbar-red.png');
+    this.load.spritesheet('enemy2', 'assets/img/enemy2.png', {frameWidth: 32, frameHeight: 32});
 
-    this.load.image('arrow', 'assets/img/arrow.png', {frameWidth: 21, frameHeight: 5});
     this.load.spritesheet('button-tower1', 'assets/img/button-tower1.png', {frameWidth: 46, frameHeight: 46});
     this.load.spritesheet('button-tower2', 'assets/img/button-tower2.png', {frameWidth: 46, frameHeight: 46});
 
@@ -35,7 +30,7 @@ var MainMenuScene = new Phaser.Class({
 
   create: function () {
     this.choice = 'play';
-    this.arrow = this.add.sprite(80, 214, 'arrow');
+    // this.arrow = this.add.sprite(80, 214, 'arrow');
 
     this.add.sprite(200, 100, 'intro');
 
@@ -48,9 +43,13 @@ var MainMenuScene = new Phaser.Class({
   },
 
   update: function (time, delta) {
+
+
     if (this.choiceTimer === 0) {
       this.choiceTimer = time + 200;
     }
+
+    /*
     if ((this.cursors.up.isDown || this.cursors.down.isDown) && time > this.choiceTimer) {
       this.choiceTimer = time + 200;
       if (this.choice === 'play') {
@@ -61,7 +60,7 @@ var MainMenuScene = new Phaser.Class({
         this.arrow.setPosition(80, 214);
         this.choice = 'play'
       }
-    }
+    }*/
 
     if (time > this.choiceTimer && (this.cursors.space.isDown || this.cursors.right.isDown)) {
       if (this.choice === 'play') {
